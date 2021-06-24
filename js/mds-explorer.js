860d3c3f0b2aece854db3ab1b1b2862406b1b26b
Olivier POTONNIEE <olivier.potonniee@thalesgroup.com>
2021-06-24 21:26:33 +0200

    initial revision

---
+++ js/mds-explorer.js
@@ -0,0 +1,71 @@
+var mdsJson;
+var id = 1;
+
+function showAuthr(e, cell) {
+  // name: e.target.textContent
+
+  $("#mds").hide();
+  $("#authr").show();
+  var jsonPretty = JSON.stringify(cell.getData(), null, '\t');
+  $("#raw-json").text(jsonPretty);
+}
+
+$("#authr-close").click(function(){
+  $("#mds").show();
+  $("#authr").hide();
+});
+
+function corsUrl(url) {
+  return "https://wtracks-cors-proxy.herokuapp.com/" + url;
+}
+
+$( function() {
+
+  let mdsUrl = corsUrl("https://mds.fidoalliance.org/");
+  $.get( mdsUrl, function( mdsJwt ) {
+    processMdsJwt(mdsJwt);
+  });
+
+  function processMdsJwt(mdsJwt) {
+    // extract payload
+    mdsJwt = mdsJwt.substring(mdsJwt.indexOf('.')+1, mdsJwt.lastIndexOf(".")) + "====";
+    // size must be modulo 4
+    let trim =  mdsJwt.length % 4;
+    mdsJwt = mdsJwt.substring(0, mdsJwt.length- trim);
+    // decode base64
+    mdsJwt = base64js.toByteArray(mdsJwt);
+    let mdsJson = JSON.parse(String.fromCharCode.apply(String, mdsJwt));
+    
+    // show JSON in console
+    console.log(mdsJson);
+
+    // build authenticators table
+    var table = new Tabulator("#mds-table", {
+      data: mdsJson.entries,
+      columns:[
+        {title:"#", formatter:function(cell, formatterParams, onRendered){ return id++; }},
+        {title:"Name", field:"metadataStatement.description", sorter:"string", headerFilter:true, cellClick:showAuthr},
+        {title:"Protocol", field:"metadataStatement.protocolFamily", sorter:"string", headerFilter:true},
+        //{title:"AAID", field:"aaid", sorter:"string", headerFilter:true},
+        {title:"Icon", field:"metadataStatement.icon", formatter:"image", witdh:150},
+        {title:"Certification", field:"statusReports", formatter:function(cell, formatterParams, onRendered){
+          let res = "", sep="";
+          $.each(cell.getValue(), function(idx,value) {res += sep + value.status; sep ="<br>"});
+          return res;
+        }},
+        {title:"User Verif.", field:"metadataStatement.userVerificationDetails", formatter:function(cell, formatterParams, onRendered){
+          let res = "", sep="";
+          $.each(cell.getValue(), function(il,line) { $.each(line, function(ii,value) {res += sep + value.userVerificationMethod; sep ="<br>"}) });
+          return res;
+        }},
+        {title:"Key Protection", field:"metadataStatement.keyProtection", formatter:function(cell, formatterParams, onRendered){
+          let res = "", sep="";
+          $.each(cell.getValue(), function(idx,value) {res += sep + value; sep ="<br>"});
+          return res;
+        }},
+        {title:"Updated", field:"timeOfLastStatusChange", sorter:"string", width:110},
+    ]
+    });
+  }
+
+});
