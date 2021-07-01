b2ca2e39808fcfc0cb4e1b75ccb6513adcc4f7df
Olivier Potonniée <74671777+opotonniee@users.noreply.github.com>
2021-07-01 16:49:20 +0200

    keep parsed json for easier debugging

--- js/mds-explorer.js
+++ js/mds-explorer.js
@@ -252,7 +252,7 @@ $(function() {
     mdsJwt = mdsJwt.substring(0, mdsJwt.length- trim);
     // decode base64
     mdsJwt = base64js.toByteArray(mdsJwt);
-    let mdsJson = JSON.parse(new TextDecoder().decode(mdsJwt));
+    mdsJson = JSON.parse(new TextDecoder().decode(mdsJwt));
 
     // show JSON in console
     console.log(mdsJson);
