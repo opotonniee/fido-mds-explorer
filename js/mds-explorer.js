8a5bd92f6f59a170c90991953aaf53015d0875d6
Olivier Potonniée <olivier.potonniee@thalesgroup.com>
2025-06-04 10:25:06 +0200

    update for mds 3.1

--- js/mds-explorer.js
+++ js/mds-explorer.js
@@ -271,9 +271,23 @@ ready(() => {
           values: [
             "NOT_FIDO_CERTIFIED",
             "FIDO_CERTIFIED",
+            "USER_VERIFICATION_BYPASS",
+            "ATTESTATION_KEY_COMPROMISE",
+            "USER_KEY_REMOTE_COMPROMISE",
+            "USER_KEY_PHYSICAL_COMPROMISE",
+            "UPDATE_AVAILABLE",
+            "REVOKED",
+            "SELF_ASSERTION_SUBMITTED",
             "FIDO_CERTIFIED_L1",
+            "FIDO_CERTIFIED_L1plus",
             "FIDO_CERTIFIED_L2",
-            "FIDO_CERTIFIED_L3"
+            "FIDO_CERTIFIED_L2plus",
+            "FIDO_CERTIFIED_L3",
+            "FIDO_CERTIFIED_L3plus",
+            "FIPS140_CERTIFIED_L1",
+            "FIPS140_CERTIFIED_L2",
+            "FIPS140_CERTIFIED_L3",
+            "FIPS140_CERTIFIED_L4"
           ]
         },
         sorter: "array",
