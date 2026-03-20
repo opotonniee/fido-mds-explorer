ed88edd5487bb30a4371f89ad125f7e122f88169
Olivier Potonniée <74671777+opotonniee@users.noreply.github.com>
2026-03-20 11:02:09 +0100

    remove tabulator dependency for convenience MDS (#15)

--- js/convenience-explorer.js
+++ js/convenience-explorer.js
@@ -4,14 +4,8 @@
 let table;
 let mdsJson;
 
-function filterNames(headerValue, rowValue/*, rowData, filterParams*/) {
-  const searched = headerValue.toLowerCase();
-  for (let v of Object.values(rowValue)) {
-    if (v.toLowerCase().indexOf(searched) >= 0) {
-      return true;
-    }
-  };
-  return false
+function updateCount() {
+  e("#table-size").innerText = document.querySelectorAll("#cmds-table tbody tr:not([hidden])").length - 1;
 }
 
 onReady(async () => {
@@ -61,75 +55,65 @@ onReady(async () => {
     };
   */
   // convert to table
-  const mdsArray = [];
+  const table = e("#cmds-table tbody");
+  let even = true;
+
+  function applyFilter() {
+    const filterName = e("#filterName").value.toLowerCase();
+    const filterLang = e("#filterLang").value.toLowerCase();
+    let header = true;
+    table.querySelectorAll("tr").forEach(tr => {
+      if (header) {
+        header = false;
+        return;
+      }
+      const friendlyNames = tr.children[1].querySelectorAll("div.friendly-name");
+      let nameMatched = false;
+      let langMatched = false;
+      friendlyNames.forEach(div => {
+        const lang = div.querySelector(".lang").innerText.toLowerCase();
+        div.hidden = filterLang && !lang.includes(filterLang);
+        langMatched |= lang.includes(filterLang);
+        if (!div.hidden) {
+          const name = div.querySelector(".name").innerText.toLowerCase();
+          nameMatched |= name.includes(filterName);
+        }
+      });
+      tr.hidden = filterName && !nameMatched || filterLang && !langMatched;
+    });
+    updateCount();
+  }
+  e("#filterName").addEventListener("input", applyFilter);
+  e("#filterLang").addEventListener("input", applyFilter);
+  e("#clear-filter").addEventListener("click", (evt) => {
+    evt.preventDefault();
+    e("#filterName").value = "";
+    e("#filterLang").value = "";
+    applyFilter();
+  });
+
+  function formatFriendlyNames(friendlyNames) {
+    if (!friendlyNames) {
+      return "";
+    }
+    return Object.entries(friendlyNames).map(([lang, name]) => `<div class="friendly-name"><span class="lang">${lang}</span>: <span class="name">${name}</span></div>`).join("");
+  }
+
   Array.from(Object.keys(mdsJson), (e, i) => {
     if (e != "no") {
-      mdsJson[e].id = e;
-      mdsArray.push(mdsJson[e]);
+      table.appendChild(newE("tr", { class: even ? "even" : "" }, `
+        <td>${e}</td>
+        <td>${formatFriendlyNames(mdsJson[e].friendlyNames)}</td>
+        <td class="img">${imageTag(mdsJson[e].icon)}</td>
+        <td class="img dark">${imageTag(mdsJson[e].iconDark)}</td>
+        <td class="img">${imageTag(mdsJson[e].providerLogoLight)}</td>
+        <td class="img dark">${imageTag(mdsJson[e].providerLogoDark)}</td>
+      </tr>`));
+      even = !even;
     }
   });
-  table = new Tabulator("#mds-table", {
-    data: mdsArray,
-    layout: "fitData",
-    selectable: false,
-    movableColumns: true,
-    //responsiveLayout: "collapse",
-    columns: [
-      {
-        title: "AAGUID",
-        field: "id",
-        sorter: "string",
-        headerFilter: true,
-      },
-      {
-        title: "Name",
-        field: "friendlyNames",
-        sorter: "string",
-        headerFilter: true,
-        headerFilterFunc: filterNames,
-        formatter: function (cell/*, formatterParams, onRendered*/) {
-          let res = "", sep = "", val = cell.getValue();
-          for (let lang of Object.keys(val) || []) { res += sep + lang + ": " + val[lang]; sep = "<br>"; }
-          return res;
-        },
-      },
-      {
-        title: "Icon (light)",
-        field: "icon",
-        formatter: imageFormatter,
-        headerMenu: hideMenu
-      },
-      {
-        title: "Icon (dark)",
-        field: "iconDark",
-        color: "#555",
-        formatter: darkImageFormatter,
-        headerMenu: hideMenu
-      },
-      {
-        title: "Provider (light)",
-        field: "providerLogoLight",
-        formatter: imageFormatter,
-        headerMenu: hideMenu
-      },
-      {
-        title: "Provider (dark)",
-        field: "providerLogoDark",
-        formatter: darkImageFormatter,
-        headerMenu: hideMenu
-      },
-    ],
-    footerElement: `<span>Payload serial: ${mdsJson.no}</span>`
-  });
-
-  e("#table-size").innerText = mdsArray.length;
-  table.on("dataFiltered", function (filters, rows) {
-    e("#table-size").innerText = rows.length;
-  });
-
-  // refresh so that icons column is properly sized
-  table.on("tableBuilt", function () {
-    table.redraw(true);
-  });
-
+  
+  e("#footer").innerHTML = `<span>Payload serial: ${mdsJson.no}</span>`;
+  e("#table-size").innerText = document.querySelectorAll("#cmds-table tbody tr").length - 1;
+  updateCount();
 });
